8a2cce998303d8774866d8e768af4aac722df32c
FBnil <nilton@online.nl>
2021-07-16 19:43:32 +0200

    Add the test to a hook and add revision in the envidencer binary

--- test/test-evidencer.pl
+++ test/test-evidencer.pl
@@ -12,6 +12,7 @@ exit if $?;
 
 
 my $TDIR = "./suits/BUILDTEST";
+my $cfg = "-c $TDIR/evidencer.cfg -s BUILDTEST";
 
 print "d=".(-d $TDIR);
 system "rm -rf $TDIR" if(-d $TDIR);
@@ -26,7 +27,7 @@ createfile($cfgF,
   'RUN_ARG=echo "%{RUNSCRIPTFQ} on %{RUNSERVERFQ} with argument %{ARG}"'
 );
 
-system $exe,'-C','-s','BUILDTEST';
+system $exe '-c',"$TDIR/evidencer.cfg",'-C','-s','BUILDTEST';
 ok( $? == 0 , 'BUILDTEST directory structure' );
 
 my %S = (
@@ -53,26 +54,26 @@ for my $d (keys %S){
   }
 }
 
-@_ = `$exe -s BUILDTEST TEST1= -d -v |grep -v '# RUN:'|grep RUN`;
+@_ = `$exe $cfg TEST1= -d -v |grep -v '# RUN:'|grep RUN`;
 say @_;
 is($#_,2-1,'two lines 1');
 like($_[0], qr/TEST1=\+\+ET on VM-ET/, 'TEST1 on ET');
 like($_[1], qr/TEST1=\+\+PR on VM-PR/, 'TEST1 on PR');
 
 
-@_ = `$exe -s BUILDTEST =++ET -d -v |grep -v '# RUN:'|grep RUN`;
+@_ = `$exe $cfg =++ET -d -v |grep -v '# RUN:'|grep RUN`;
 say @_;
 is($#_,2-1,'two lines 2');
 like($_[0], qr/TEST1=\+\+ET on VM-ET/, 'TEST1 on ET');
 like($_[1], qr/TEST2=\+\+ET on VM-ET/, 'TEST2 on ET');
 
 
-@_ = `$exe -s BUILDTEST TEST1=++PR -d -v |grep -v '# RUN:'|grep RUN`;
+@_ = `$exe $cfg TEST1=++PR -d -v |grep -v '# RUN:'|grep RUN`;
 say @_;
 is($#_,1-1,'one line');
 like($_[0], qr/TEST1=\+\+PR on VM-PR/, 'TEST1 on PR');
 
-@_ = `$exe -s BUILDTEST TEST2=++PR+ -d -v |grep -v '# RUN:'|grep RUN`;
+@_ = `$exe $cfg TEST2=++PR+ -d -v |grep -v '# RUN:'|grep RUN`;
 say @_;
 is($#_,2-1,'two lines dmz');
 like($_[0], qr/TEST2=\+\+PR\+ on VM-PR/, 'TEST2 on PR');
@@ -81,7 +82,7 @@ like($_[1], qr/TEST2=\+\+PR\+ on VM-PR-DMZ/, 'TEST2 on PR with dmz');
 subtest 'Grouping' => sub {
   plan tests => 2;
 
-  @_ = `$exe -s BUILDTEST TEST2=++PR+ -g -d -v |grep -v '# RUN:'|grep RUN`;
+  @_ = `$exe $cfg TEST2=++PR+ -g -d -v |grep -v '# RUN:'|grep RUN`;
   say @_;
   is($#_,1-1,'one line grouped');
   like($_[0], qr/TEST2=\+\+PR\+ on VM-PR  VM-PR-DMZ/, 'grouped');
@@ -90,11 +91,11 @@ subtest 'Grouping' => sub {
 subtest 'Folding1' => sub {
   plan tests => 3;
 
-  @_ = `$exe -s BUILDTEST =++ET -f -d -v |grep -v '# RUN:'|grep -e RUN -e echo`;
+  @_ = `$exe $cfg =++ET -f -d -v |grep -v '# RUN:'|grep -e RUN -e echo`;
   say @_;
   is($#_,2-1,'two lines folded');
   like($_[0], qr/TEST1=\+\+ET  TEST2=\+\+ET on VM-ET/, 'folded');
-  like($_[1], qr!/home/nilton/CODE/PERL/EVIDENCER/suits/BUILDTEST/scripts/TEST1=\+\+ET  /home/nilton/CODE/PERL/EVIDENCER/suits/BUILDTEST/scripts/TEST2=\+\+ET on /home/nilton/CODE/PERL/EVIDENCER/suits/BUILDTEST/servers/VM-ET"!, 'folded run');
+  like($_[1], qr!/home/nilton/CODE/PERL/EVIDENCER/suits/BUILDTEST/scripts/TEST1=\+\+ET  /home/nilton/CODE/PERL/EVIDENCER/suits/BUILDTEST/scripts/TEST2=\+\+ET on /home/nilton/CODE/PERL/EVIDENCER/suits/BUILDTEST/servers/VM-ET"!i, 'folded run');
   
 };
 
@@ -102,18 +103,18 @@ subtest 'Folding1' => sub {
 subtest 'bundling parameters' => sub {
   plan tests => 3;
 
-  @_ = `$exe -s BUILDTEST =++ET -fdv |grep -v '# RUN:'|grep -e RUN -e echo`;
+  @_ = `$exe $cfg =++ET -fdv |grep -v '# RUN:'|grep -e RUN -e echo`;
   say @_;
   is($#_,2-1,'two lines bundled');
   like($_[0], qr/TEST1=\+\+ET  TEST2=\+\+ET on VM-ET/, 'bundling');
-  like($_[1], qr!/home/nilton/CODE/PERL/EVIDENCER/suits/BUILDTEST/scripts/TEST1=\+\+ET  /home/nilton/CODE/PERL/EVIDENCER/suits/BUILDTEST/scripts/TEST2=\+\+ET on /home/nilton/CODE/PERL/EVIDENCER/suits/BUILDTEST/servers/VM-ET"!, 'bundling run');
+  like($_[1], qr!/home/nilton/CODE/PERL/EVIDENCER/suits/BUILDTEST/scripts/TEST1=\+\+ET  /home/nilton/CODE/PERL/EVIDENCER/suits/BUILDTEST/scripts/TEST2=\+\+ET on /home/nilton/CODE/PERL/EVIDENCER/suits/BUILDTEST/servers/VM-ET"!i, 'bundling run');
   
 };
 
 
 subtest 'serverregexp' => sub {
   plan tests => 2;
-  @_ = `$exe -s BUILDTEST =++ET\@svr1et -d -v |grep -v '# RUN:'|grep RUN`;
+  @_ = `$exe $cfg =++ET\@svr1et -d -v |grep -v '# RUN:'|grep RUN`;
   say @_;
   like($_[0], qr/BUILDTEST:TEST1=\+\+ET on BUILDTEST#TEST1=\+\+ET#VM-ET/, 'newserverfile1');
   like($_[1], qr/BUILDTEST:TEST2=\+\+ET on BUILDTEST#TEST2=\+\+ET#VM-ET/, 'newserverfile2');
@@ -121,7 +122,7 @@ subtest 'serverregexp' => sub {
 
 subtest 'redefine' => sub {
   plan tests => 2;
-  @_ = `$exe -s BUILDTEST =++ET\@svr1et -d -v -r ALTERNATIVE1 |grep ^on`;
+  @_ = `$exe $cfg =++ET\@svr1et -d -v -r ALTERNATIVE1 |grep ^on`;
   say @_;
   like($_[0], qr!on /.*/BUILDTEST#TEST1=\+\+ET#VM-ET#\d+ run /.*/BUILDTEST/scripts/TEST1=\+\+ET!, 'alternative1a');
   like($_[1], qr!on /.*/BUILDTEST#TEST2=\+\+ET#VM-ET#\d+ run /.*/BUILDTEST/scripts/TEST2=\+\+ET!, 'alternative1b');
@@ -129,28 +130,28 @@ subtest 'redefine' => sub {
 
 subtest 'argument1' => sub {
   plan tests => 2;
-  @_ = `$exe -s BUILDTEST TEST2=++ET\@svr1et -d -v -- --fantastic 4 |grep ^echo`;
+  @_ = `$exe $cfg TEST2=++ET\@svr1et -d -v -- --fantastic 4 |grep ^echo`;
   say @_;
   like($_[0], qr!on /.*/BUILDTEST#TEST2=\+\+ET#VM-ET#\d+ with argument --fantastic 4!, 'argument1a');
-  @_ = `$exe -s BUILDTEST TEST2=++ET\@svr1et -d -v -a incredible |grep ^echo`;
+  @_ = `$exe $cfg TEST2=++ET\@svr1et -d -v -a incredible |grep ^echo`;
   say @_;
   like($_[0], qr!on /.*/BUILDTEST#TEST2=\+\+ET#VM-ET#\d+ with argument incredible!, 'argument2a');
 };
 
-@_ = `$exe -s BUILDTEST =# -dv |grep -v '# RUN:'|grep RUN`;
+@_ = `$exe $cfg =# -dv |grep -v '# RUN:'|grep RUN`;
 say @_;
 is($#_,2-1,'hash is latest');
 like($_[0], qr/BUILDTEST:TEST1=\+\+ET on VM-ET/, 'TEST1 on ET');
 like($_[1], qr/BUILDTEST:TEST2=\+\+PR\+ on VM-ET/, 'TEST2 on ET');
 
-@_ = `$exe -s BUILDTEST =# -dv -l VM-PR |grep -v '# RUN:'|grep RUN`;
+@_ = `$exe $cfg =# -dv -l VM-PR |grep -v '# RUN:'|grep RUN`;
 say @_;
 is($#_,2-1,'Test --loop 1');
 unlike($_[0], qr/DMZ/, 'TEST1 on PR through --loop 1 - no DMZ match');
 unlike($_[0], qr/BUILDTEST:TEST1=\+\+ET on VM-PR/, 'TEST1 on PR through --loop 1');
 like($_[1], qr/BUILDTEST:TEST2=\+\+PR\+ on VM-PR/, 'TEST2 on PR through --loop 1');
 
-@_ = `$exe -s BUILDTEST =# -v -l VM-PR |grep -v '# RUN:'|grep RUN`;
+@_ = `$exe $cfg =# -v -l VM-PR |grep -v '# RUN:'|grep RUN`;
 say @_;
 is($#_,2-1,'Test --loop 2');
 unlike($_[0], qr/DMZ/, 'TEST1 on PR through --loop 2 - no DMZ match');
@@ -159,15 +160,15 @@ like($_[1], qr/BUILDTEST:TEST2=\+\+PR\+ on VM-PR/, 'TEST2 on PR through --loop 2
 
 subtest 'LOOPs are same as not using loops' => sub {
   plan tests => 6;
-	my @ONE= `$exe -s BUILDTEST =# -dv -l VM-PR |grep -v '# RUN:'|grep RUN`;
-	my @TWO= `$exe -s BUILDTEST =VM-PR -dv  |grep -v '# RUN:'|grep RUN`;
+	my @ONE= `$exe $cfg =# -dv -l VM-PR |grep -v '# RUN:'|grep RUN`;
+	my @TWO= `$exe $cfg =VM-PR -dv  |grep -v '# RUN:'|grep RUN`;
 	while($a=shift(@ONE)){
 		$b = shift @TWO;
 		is($a,$b,"Loop v/s noloop one element is same");
 	}
 
-	my @ONE= `$exe -s BUILDTEST =# -dv -l VM-PR,VM-ET |grep -v '# RUN:'|grep RUN`;
-	my @TWO= `$exe -s BUILDTEST =VM-PR,VM-ET -dv  |grep -v '# RUN:'|grep RUN`;
+	my @ONE= `$exe $cfg =# -dv -l VM-PR,VM-ET |grep -v '# RUN:'|grep RUN`;
+	my @TWO= `$exe $cfg =VM-PR,VM-ET -dv  |grep -v '# RUN:'|grep RUN`;
 	while($a=shift(@ONE)){
 		$b = shift @TWO;
 		is($a,$b,"Loop v/s noloop two elements is same");
@@ -176,12 +177,12 @@ subtest 'LOOPs are same as not using loops' => sub {
 
 subtest 'Test accuracy of new regexp' => sub {
   plan tests => 8;
-	@_ = `$exe -s BUILDTEST 'TEST[12]=+PR' -v |grep -v '# RUN:'|grep RUN`;
+	@_ = `$exe $cfg 'TEST[12]=+PR' -v |grep -v '# RUN:'|grep RUN`;
 	say @_;
 	is($#_,2-1,'two lines for output accuracy');
 	like($_[0], qr/BUILDTEST:TEST1=\+\+PR on VM-PR/, 'TEST1 accuracy test with new rexexp PR');
 	like($_[1], qr/BUILDTEST:TEST2=\+\+PR\+ on VM-PR/, 'TEST2 accuracy test with new rexexp PR');
-	@_ = `$exe -s BUILDTEST 'TEST[12]=+PR,+ET' -v |grep -v '# RUN:'|grep RUN`;
+	@_ = `$exe $cfg 'TEST[12]=+PR,+ET' -v |grep -v '# RUN:'|grep RUN`;
 	say @_;
 	is($#_,4-1,'four lines for output accuracy');
 	like($_[1], qr/BUILDTEST:TEST1=\+\+PR on VM-PR/, 'TEST1 accuracy test with new rexexp PR - multiple');
